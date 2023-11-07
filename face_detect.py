9f7467346035f2365213450e359cde335d218d09
Rich Bui <rbui54321.com>
2023-11-07 02:54:55 -0800

    Added in neural model

--- face_detect.py
+++ face_detect.py
@@ -7,6 +7,10 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import json
 import albumentations as A
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Conv2D, Dense, GlobalMaxPooling2D, MaxPooling2D, Flatten, Add, Dropout
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.models import load_model
 
 # getting the image path data/images
 # image_path = os.path.join('data', 'images')
@@ -35,8 +39,7 @@ import albumentations as A
 # for gpu in gpus:
 #     tf.config.experimental.set_memory_growth(gpu, True)
 
-# gpu_list = tf.config.list_physical_devices('GPU')
-# print("Available GPUs:", gpu_list)
+# print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
 # from tensorflow.python.client import device_lib
 
@@ -132,7 +135,7 @@ cv2.rectangle(augmented['image'], tuple(np.multiply(augmented['bboxes'][0][:2], 
              tuple(np.multiply(augmented['bboxes'][0][2:], [450, 450]).astype(int)), (255, 0, 0), 2)
 
 # display the image using imshow()
-plt.imshow(augmented['image'])
+#plt.imshow(augmented['image'])
 
 # show image in a graphical window
 #plt.show()
@@ -204,47 +207,227 @@ def load_labels(label_path):
         label = json.load(file)
     return [label['class']], label['bbox']
 
-# pretrained file for face
-# cascade_path = pathlib.Path(cv2.__file__).parent.absolute() / "data/haarcascade_frontalface_default.xml"
-# print(cascade_path)
-# clf = cv2.CascadeClassifier(str(cascade_path))
+train_labels = tf.data.Dataset.list_files('augmented_data\\train\\labels\\*.json', shuffle = False)
+train_labels = train_labels.map(lambda x: tf.py_function(load_labels, [x], [tf.uint8, tf.float16]))
 
-#pretrained file for eyes
-# cle = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye.xml")
+test_labels = tf.data.Dataset.list_files('augmented_data\\test\\labels\\*.json', shuffle = False)
+test_labels = test_labels.map(lambda x: tf.py_function(load_labels, [x], [tf.uint8, tf.float16]))
 
-# camera = cv2.VideoCapture(0)
+val_labels = tf.data.Dataset.list_files('augmented_data\\val\\labels\\*.json', shuffle = False)
+val_labels = val_labels.map(lambda x: tf.py_function(load_labels, [x], [tf.uint8, tf.float16]))
 
-# roi_gray = None
-# while True:
-#     # reading image in grayscale mode
-#     _, frame = camera.read()
-#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#     faces = clf.detectMultiScale(
-#         gray,
-#         scaleFactor=1.1,
-#         minNeighbors=5,
-#         minSize=(30, 30),
-#         flags=cv2.CASCADE_SCALE_IMAGE
-#     )
-    # draw rectangle in a face
-    # for (x, y, width, height) in faces:
-    #     cv2.rectangle(frame, (x, y), (x+width, y+height), (255, 255, 0), 2)
-    #     roi_gray = gray[y:y+height, x:x+width]
-    #     roi_color = frame[y:y+height, x:x+width]
+print(train_labels.as_numpy_iterator().next())
+print(len(train_images), len(train_labels), len(test_images), len(test_labels), len(val_images), len(val_labels))
 
+train = tf.data.Dataset.zip((train_images, train_labels))
+train = train.shuffle(8000)
+train = train.batch(8)
+train = train.prefetch(4)
+test = tf.data.Dataset.zip((test_images, test_labels))
+test = test.shuffle(2000)
+test = test.batch(8)
+test = test.prefetch(4)
+val = tf.data.Dataset.zip((val_images, val_labels))
+val = val.shuffle(1800)
+val = val.batch(8)
+val = val.prefetch(4)
+
+# print(train.as_numpy_iterator().next()[1])
+
+# data_samples = train.as_numpy_iterator()
+# res = data_samples.next()
+# fig, ax = plt.subplots(ncols=4, figsize=(20,20))
+# for idx in range(4): 
+#     sample_image = res[0][idx]
+#     sample_coords = res[1][1][idx]
     
-    # detect the eyes of different size 
-    # eyes = cle.detectMultiScale(roi_gray)
+#     cv2.rectangle(sample_image, 
+#                   tuple(np.multiply(sample_coords[:2], [120,120]).astype(int)),
+#                   tuple(np.multiply(sample_coords[2:], [120,120]).astype(int)), 
+#                         (255,0,0), 2)
 
-    # # draw rectangle in eyes
-    # for (eyes_x, eyes_y, eyes_width, eyes_height) in eyes:
-    #     cv2.rectangle(roi_color, (eyes_x, eyes_y), (eyes_x + eyes_width, eyes_y + eyes_height), (0, 127, 255), 2)
+#     ax[idx].imshow(sample_image)
+# plt.show()
 
-    # cv2.imshow("Faces", frame)
-    # Quits the program if user presses q on the keyboard
-    # if cv2.waitKey(1) == ord("q"):
-    #     break
+vgg = VGG16(include_top=False)
+vgg.summary()
 
-# closes camera capture and deallocate memory.
-# camera.release()
-# cv2.destroyAllWindows()
+# Building VGG neural network
+def build_model(): 
+    input_layer = Input(shape=(120,120,3))
+    
+    vgg = VGG16(include_top=False)(input_layer)
+
+    # Classification Model  
+    f1 = GlobalMaxPooling2D()(vgg)
+    class1 = Dense(2048, activation='relu')(f1)
+    class2 = Dense(1, activation='sigmoid')(class1)
+    
+    # Bounding box model
+    f2 = GlobalMaxPooling2D()(vgg)
+    regress1 = Dense(2048, activation='relu')(f2)
+    regress2 = Dense(4, activation='sigmoid')(regress1)
+    
+    facetracker = Model(inputs=input_layer, outputs=[class2, regress2])
+    return facetracker
+
+facetracker = build_model()
+facetracker.summary()
+X, y = train.as_numpy_iterator().next()
+X.shape
+classes, coords = facetracker.predict(X)
+print(classes, coords)
+
+batches_per_epoch = len(train)
+initial_learning_rate = 0.0001
+
+# Define a learning rate schedule with ExponentialDecay
+lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+initial_learning_rate, decay_steps = batches_per_epoch, decay_rate = 0.75, staircase = False)
+
+opt = tf.keras.optimizers.Adam(learning_rate = lr_schedule)
+
+def localization_loss(y_true, yhat):            
+    delta_coord = tf.reduce_sum(tf.square(y_true[:,:2] - yhat[:,:2]))
+                  
+    h_true = y_true[:,3] - y_true[:,1] 
+    w_true = y_true[:,2] - y_true[:,0] 
+
+    h_pred = yhat[:,3] - yhat[:,1] 
+    w_pred = yhat[:,2] - yhat[:,0] 
+    
+    delta_size = tf.reduce_sum(tf.square(w_true - w_pred) + tf.square(h_true-h_pred))
+    
+    return delta_coord + delta_size
+
+
+classloss = tf.keras.losses.BinaryCrossentropy()
+regressloss = localization_loss
+
+class FaceTracker(Model): 
+    def __init__(self, eyetracker,  **kwargs): 
+        super().__init__(**kwargs)
+        self.model = eyetracker
+
+    def compile(self, opt, classloss, localizationloss, **kwargs):
+        super().compile(**kwargs)
+        self.closs = classloss
+        self.lloss = localizationloss
+        self.opt = opt
+    
+    def train_step(self, batch, **kwargs): 
+        
+        X, y = batch
+        
+        with tf.GradientTape() as tape: 
+            classes, coords = self.model(X, training=True)
+            
+            batch_classloss = self.closs(y[0], classes)
+            batch_localizationloss = self.lloss(tf.cast(y[1], tf.float32), coords)
+            
+            total_loss = batch_localizationloss+0.5*batch_classloss
+            
+            grad = tape.gradient(total_loss, self.model.trainable_variables)
+        
+        opt.apply_gradients(zip(grad, self.model.trainable_variables))
+        
+        return {"total_loss":total_loss, "class_loss":batch_classloss, "regress_loss":batch_localizationloss}
+    
+    def test_step(self, batch, **kwargs): 
+        X, y = batch
+        
+        classes, coords = self.model(X, training=False)
+        
+        batch_classloss = self.closs(y[0], classes)
+        batch_localizationloss = self.lloss(tf.cast(y[1], tf.float32), coords)
+        total_loss = batch_localizationloss+0.5*batch_classloss
+        
+        return {"total_loss":total_loss, "class_loss":batch_classloss, "regress_loss":batch_localizationloss}
+        
+    def call(self, X, **kwargs): 
+        return self.model(X, **kwargs)
+model = FaceTracker(facetracker)
+model.compile(opt, classloss, regressloss)
+
+logdir='logs'
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
+# hist = model.fit(train, epochs=10, validation_data=val, callbacks=[tensorboard_callback])
+
+# hist.history
+# fig, ax = plt.subplots(ncols=3, figsize=(20,5))
+
+# ax[0].plot(hist.history['total_loss'], color='teal', label='loss')
+# ax[0].plot(hist.history['val_total_loss'], color='orange', label='val loss')
+# ax[0].title.set_text('Loss')
+# ax[0].legend()
+
+# ax[1].plot(hist.history['class_loss'], color='teal', label='class loss')
+# ax[1].plot(hist.history['val_class_loss'], color='orange', label='val class loss')
+# ax[1].title.set_text('Classification Loss')
+# ax[1].legend()
+
+# ax[2].plot(hist.history['regress_loss'], color='teal', label='regress loss')
+# ax[2].plot(hist.history['val_regress_loss'], color='orange', label='val regress loss')
+# ax[2].title.set_text('Regression Loss')
+# ax[2].legend()
+
+# plt.show()
+
+# making predictions on test set
+test_data = test.as_numpy_iterator()
+test_sample = test_data.next()
+yhat = facetracker.predict(test_sample[0])
+fig, ax = plt.subplots(ncols=4, figsize=(20,20))
+for idx in range(4): 
+    sample_image = test_sample[0][idx]
+    sample_coords = yhat[1][idx]
+    
+    if yhat[0][idx] > 0.9:
+        cv2.rectangle(sample_image, 
+                      tuple(np.multiply(sample_coords[:2], [120,120]).astype(int)),
+                      tuple(np.multiply(sample_coords[2:], [120,120]).astype(int)), 
+                            (255,0,0), 2)
+    
+    ax[idx].imshow(sample_image)
+plt.show()
+
+
+facetracker.save('facetracker.h5')
+facetracker = load_model('facetracker.h5')
+
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    _ , frame = cap.read()
+    frame = frame[50:500, 50:500,:]
+    
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    resized = tf.image.resize(rgb, (120,120))
+    
+    yhat = facetracker.predict(np.expand_dims(resized/255,0))
+    sample_coords = yhat[1][0]
+    
+    if yhat[0] > 0.5: 
+        # Controls the main rectangle
+        cv2.rectangle(frame, 
+                      tuple(np.multiply(sample_coords[:2], [450,450]).astype(int)),
+                      tuple(np.multiply(sample_coords[2:], [450,450]).astype(int)), 
+                            (255,0,0), 2)
+        # Controls the label rectangle
+        cv2.rectangle(frame, 
+                      tuple(np.add(np.multiply(sample_coords[:2], [450,450]).astype(int), 
+                                    [0,-30])),
+                      tuple(np.add(np.multiply(sample_coords[:2], [450,450]).astype(int),
+                                    [80,0])), 
+                            (255,0,0), -1)
+        
+        # Controls the text rendered
+        cv2.putText(frame, 'face', tuple(np.add(np.multiply(sample_coords[:2], [450,450]).astype(int),
+                                               [0,-5])),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+    
+    cv2.imshow('EyeTrack', frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
